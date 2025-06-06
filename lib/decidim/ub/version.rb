3a18ec27e32f9dcfd7b21efd826125315d97fb93
Harrison Hassig <harrisonhassig@gmail.com>
2025-06-06 13:43:31 +0200

    define version 0.2.0

--- lib/decidim/ub/version.rb
+++ lib/decidim/ub/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-meetings version.
   module Ub
-    VERSION = "0.1.1"
+    VERSION = "0.2.0"
     DECIDIM_VERSION = "~> 0.29.0"
     COMPAT_DECIDIM_VERSION = [">= 0.29.0", "< 0.30"].freeze
   end
